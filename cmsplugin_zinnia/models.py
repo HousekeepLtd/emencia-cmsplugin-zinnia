1369690317828ca27b35f4b893fd51067cefc3f6
Gregory Warner <64416037+worthwhile-gwarner@users.noreply.github.com>
2020-06-26 11:40:23 -0400

    Update to models for Django 3

--- cmsplugin_zinnia/models.py
+++ cmsplugin_zinnia/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.utils import six
 from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 
 from cms.models.pluginmodel import CMSPlugin
 
